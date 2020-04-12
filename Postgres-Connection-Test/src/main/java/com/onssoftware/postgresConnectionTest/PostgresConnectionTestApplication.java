26ba0e084c44d7b9bb0526d38bf983ad8d92fee6
Md. Najmul Hasan <najmul@qianalysis.com>
2020-04-12 14:11:28 +0600

    Added Postgres Connection Test project

---
+++ Postgres-Connection-Test/src/main/java/com/onssoftware/postgresConnectionTest/PostgresConnectionTestApplication.java
@@ -0,0 +1,22 @@
+package com.onssoftware.postgresConnectionTest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PostgresConnectionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresConnectionTestApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner dbTest(AccountRepository accRepo) {
+		return args -> {
+			Account acc = new Account();
+			accRepo.save(acc);
+		};
+	}
+}
