5c908b9a2efccae193a6e13593bf8332a9fd5934
Md. Najmul Hasan <najmul@qianalysis.com>
2020-06-13 07:46:22 +0600

    Added new project files

--- RabbitMQ-Test/src/main/java/com/onssoftware/rabbitMqTest/helloWorld/springWay/Consumer.java
+++ RabbitMQ-Test/src/main/java/com/onssoftware/rabbitMqTest/helloWorld/springWay/Consumer.java
@@ -3,7 +3,7 @@ package com.onssoftware.rabbitMqTest.helloWorld.springWay;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Consumer {
 
     @RabbitListener(queues = "simpleQueue")
