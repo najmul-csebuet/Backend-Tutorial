f425d5a69737877607d0f473f7aae68ab39c2a85
Md. Najmul Hasan <najmul@qianalysis.com>
2020-07-26 12:03:39 +0600

    Added email sending projects

---
+++ Unit-Testing/src/test/java/com/onssoftware/unitTesting/TestJUnit.java
@@ -0,0 +1,12 @@
+package com.onssoftware.unitTesting;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestJUnit {
+
+    @Test
+    public void checkingAdd() {
+        assertEquals(8,4+4);
+    }
+}
