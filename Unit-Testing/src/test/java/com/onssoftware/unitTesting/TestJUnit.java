e3b80659f1a9f4cf3ba6ded22680bbe75966a219
Md. Najmul Hasan <najmul@qianalysis.com>
2020-09-16 21:58:31 +0600

    Added JWT Demo Project With Spring Boot and Spring Security.

--- Unit-Testing/src/test/java/com/onssoftware/unitTesting/TestJUnit.java
+++ Unit-Testing/src/test/java/com/onssoftware/unitTesting/TestJUnit.java
@@ -1,12 +1,16 @@
 package com.onssoftware.unitTesting;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//@SpringBootTest
 public class TestJUnit {
 
     @Test
     public void checkingAdd() {
+        System.out.println("Bismillah");
         assertEquals(8,4+4);
     }
 }
