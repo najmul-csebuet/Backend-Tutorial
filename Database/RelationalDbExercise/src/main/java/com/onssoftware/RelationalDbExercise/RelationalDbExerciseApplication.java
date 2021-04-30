fd773888739aee1f46f0c4b83bac928242318612
Md. Najmul Hasan <najmul@qianalysis.com>
2021-04-30 16:20:10 +0600

    Added docker compose file for starting postgres db

---
+++ Database/RelationalDbExercise/src/main/java/com/onssoftware/RelationalDbExercise/RelationalDbExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.onssoftware.RelationalDbExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDbExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalDbExerciseApplication.class, args);
+	}
+
+}
