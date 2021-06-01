03a675c96bd0a0fe0ebb4f7038d652ce9408ab1e
najmul.csebuet <najmul.csebuet@gmail.com>
2021-06-01 12:54:30 +0600

    one to one

--- Database/JavaProjects/OneToOneMapping/src/main/java/com/onssoftware/OneToOneMapping/OneToOneMappingApplication.java
+++ Database/JavaProjects/OneToOneMapping/src/main/java/com/onssoftware/OneToOneMapping/OneToOneMappingApplication.java
@@ -1,7 +1,13 @@
 package com.onssoftware.OneToOneMapping;
 
+import com.onssoftware.OneToOneMapping.model.Account;
+import com.onssoftware.OneToOneMapping.model.Address;
+import com.onssoftware.OneToOneMapping.repository.AccountRepository;
+import com.onssoftware.OneToOneMapping.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OneToOneMappingApplication {
@@ -10,4 +16,18 @@ public class OneToOneMappingApplication {
 		SpringApplication.run(OneToOneMappingApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDb(AccountService accountService) {
+		return args -> {
+
+			var address = new Address();
+			address.setName("Mymensingh");
+
+			var acc = new Account();
+			acc.setName("Saad");
+			acc.setAddress(address);
+
+			accountService.save(acc);
+		};
+	}
 }
---
+++ Database/JavaProjects/OneToOneMapping/src/main/java/com/onssoftware/OneToOneMapping/model/Account.java
@@ -0,0 +1,23 @@
+package com.onssoftware.OneToOneMapping.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Address address;
+}
