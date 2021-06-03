287509179eff84c6fe25515891ab9873d205beaa
Md. Najmul Hasan <najmul@qianalysis.com>
2021-06-03 16:23:16 +0600

    hb tutorial

--- Database/JavaProjects/DataJpaTutorial/src/main/java/com/onssoftware/DataJpaTutorial/OneToOne/AccountService.java
+++ Database/JavaProjects/DataJpaTutorial/src/main/java/com/onssoftware/DataJpaTutorial/OneToOne/AccountService.java
@@ -1,15 +1,21 @@
 package com.onssoftware.DataJpaTutorial.OneToOne;
 
-import com.onssoftware.DataJpaTutorial.OneToOne.Account;
-import com.onssoftware.DataJpaTutorial.OneToOne.AccountRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class AccountService {
     // will be autowired by @RequiredArgsConstructor
+
+    private final EntityManager entityManager;
     private final AccountRepository accountRepository;
+
+    public AccountService(EntityManager entityManager, AccountRepository accountRepository) {
+        this.entityManager = entityManager;
+        this.accountRepository = accountRepository;
+    }
 
     public Account save(Account acc) {
         return accountRepository.save(acc);
