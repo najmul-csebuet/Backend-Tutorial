273c9e9c85a5c4fb4fc1e1ecb704a0c21cb3065f
Md. Najmul Hasan <najmul@qianalysis.com>
2021-07-07 09:40:58 +0600

    Testing TestPropertuSource Annotation

--- Database/JavaProjects/DataJpaTutorial/src/main/java/com/onssoftware/DataJpaTutorial/OneToOne/Account.java
+++ Database/JavaProjects/DataJpaTutorial/src/main/java/com/onssoftware/DataJpaTutorial/OneToOne/Account.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -16,11 +19,14 @@ public class Account {
     @Setter(AccessLevel.NONE)
     private Long id;
 
+    @NotEmpty(message = "Name can't be empty")
     private String name;
 
     //@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     //@OneToOne(cascade = {CascadeType.ALL})
     //@JoinColumn(name = "Test")
-    @OneToOne(cascade = {CascadeType.PERSIST}, fetch = FetchType.LAZY)
+    @OneToOne(cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    @NotBlank
+    @Column(name = "address", nullable = false)
     private Address address;
 }
